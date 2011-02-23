d18080cfdce22920d1c21d731917c9585f3d5808
leonhad@gmail.com <leonhad@gmail.com@b074b155-7ff8-1dec-5e99-c3ab0efeb2e8>
2011-02-23 20:43:23 +0000

    Path stroking test

--- trunk/jalapaquery/src/jalapaquery/mainwindow.cpp
+++ trunk/jalapaquery/src/jalapaquery/mainwindow.cpp
@@ -12,6 +12,22 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->mainView->setScene(scene);
 
     connect(ui->actionAddTable, SIGNAL(triggered()), this, SLOT(addTable()));
+
+    TestItem *item1 = new TestItem;
+    TestItem *item2 = new TestItem;
+
+    item1->setPos(-100, -10);
+    item2->setPos(100, 10);
+
+    QPen pen(Qt::black, 1, Qt::SolidLine, Qt::SquareCap, Qt::RoundJoin);
+    QPainterPath path;
+    path.moveTo(item1->pos().x() + item1->boundingRect().width(), item1->pos().y() + item1->boundingRect().height() / 2);
+    path.lineTo(item2->pos().x(), item2->y() + item2->boundingRect().height() / 2); // lazy
+
+    scene->addPath(path, pen);
+
+    scene->addItem(item1);
+    scene->addItem(item2);
 }
 
 MainWindow::~MainWindow()
