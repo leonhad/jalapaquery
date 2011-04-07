474fd028ae964aab18c9bd5e263b82b9cdafc5bc
leonhad@gmail.com <leonhad@gmail.com@b074b155-7ff8-1dec-5e99-c3ab0efeb2e8>
2011-04-07 20:30:53 +0000

    Added a initial internacionalization

---
+++ trunk/jalapaquery/src/jalapaquery/jalapaquery_pt.ts
@@ -0,0 +1,373 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_PT">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="aboutdialog.ui" line="14"/>
+        <source>About JalapaQuery</source>
+        <translation>Sobre JalapaQuery</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="33"/>
+        <source>JalapaQuery (version)</source>
+        <translation>JalapaQuery (versão)</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>JalapaQuery</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="66"/>
+        <source>&amp;File</source>
+        <translation>&amp;Arquivo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="70"/>
+        <source>Recent Files</source>
+        <translation>Arquivos Recentes</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="91"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Editar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="108"/>
+        <source>&amp;View</source>
+        <translation>&amp;Visão</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="112"/>
+        <source>Toolbars</source>
+        <translation>Barra de Ferramentas</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="117"/>
+        <source>Zoom</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="133"/>
+        <source>Docks</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="143"/>
+        <source>&amp;Model</source>
+        <translation>&amp;Modelo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="148"/>
+        <source>&amp;Tools</source>
+        <translation>&amp;Ferramentas</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="153"/>
+        <source>&amp;Help</source>
+        <translation>A&amp;juda</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="175"/>
+        <source>File</source>
+        <translation>Arquivo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="209"/>
+        <source>Project</source>
+        <translation>Projeto</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="242"/>
+        <source>Tools</source>
+        <translation>Ferramentas</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="273"/>
+        <source>Page 1</source>
+        <translation>Página 1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="286"/>
+        <source>Page 2</source>
+        <translation>Página 2</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="296"/>
+        <source>Editing</source>
+        <translation>Edição</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="324"/>
+        <source>Help</source>
+        <translation>Ajuda</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="342"/>
+        <source>&amp;About</source>
+        <translation>&amp;Sobre</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="350"/>
+        <source>E&amp;xit</source>
+        <translation>Sa&amp;ir</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="353"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="361"/>
+        <source>&amp;Preferences</source>
+        <translation>&amp;Preferências</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="373"/>
+        <source>Index</source>
+        <translation>Índice</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="376"/>
+        <source>F1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="385"/>
+        <source>&amp;New</source>
+        <translation>&amp;Novo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="388"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="397"/>
+        <source>&amp;Open...</source>
+        <translation>&amp;Abrir...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="400"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="409"/>
+        <source>Save</source>
+        <translation>Salvar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="412"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="417"/>
+        <source>Close</source>
+        <translation>Fechar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="420"/>
+        <source>Ctrl+W</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="425"/>
+        <location filename="mainwindow.ui" line="428"/>
+        <source>Save As...</source>
+        <translation>Salvar Como...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="431"/>
+        <source>Ctrl+Shift+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="440"/>
+        <source>Print</source>
+        <translation>Imprimir</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="443"/>
+        <source>Ctrl+P</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="448"/>
+        <source>Page Setup</source>
+        <translation>Configurar Página</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="453"/>
+        <source>Import</source>
+        <translation>Importar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="458"/>
+        <source>Export</source>
+        <translation>Exportar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="467"/>
+        <source>Undo</source>
+        <translation>Desfazer</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="470"/>
+        <source>Ctrl+Z</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="479"/>
+        <source>Redo</source>
+        <translation>Refazer</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="482"/>
+        <source>Ctrl+Shift+Z</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="491"/>
+        <source>Cut</source>
+        <translation>Cortar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="494"/>
+        <source>Ctrl+X</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="503"/>
+        <source>Copy</source>
+        <translation>Copiar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="506"/>
+        <source>Ctrl+C</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="515"/>
+        <source>Paste</source>
+        <translation>Colar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="518"/>
+        <source>Ctrl+V</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="527"/>
+        <source>Find...</source>
+        <translation>Procurar...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="530"/>
+        <source>Ctrl+F</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="535"/>
+        <source>Select All</source>
+        <translation>Selecionar Tudo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="538"/>
+        <source>Ctrl+A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="547"/>
+        <source>Zoom In</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="556"/>
+        <source>Zoom Out</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="561"/>
+        <source>Fit To Window</source>
+        <translation>Ajustar a Janela</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="566"/>
+        <source>50%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="571"/>
+        <source>75%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="576"/>
+        <source>100%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="581"/>
+        <source>125%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="586"/>
+        <source>150%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="591"/>
+        <source>175%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="596"/>
+        <source>200%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="601"/>
+        <source>About Plugins...</source>
+        <translation>Sobre Plugins...</translation>
+    </message>
+</context>
+<context>
+    <name>NewFileDialog</name>
+    <message>
+        <location filename="newfiledialog.ui" line="14"/>
+        <source>New</source>
+        <translation>Novo
+</translation>
+    </message>
+</context>
+<context>
+    <name>PluginDialog</name>
+    <message>
+        <location filename="plugindialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Diálogo</translation>
+    </message>
+    <message>
+        <location filename="plugindialog.cpp" line="14"/>
+        <source>Name</source>
+        <translation>Nome</translation>
+    </message>
+    <message>
+        <location filename="plugindialog.cpp" line="14"/>
+        <source>Version</source>
+        <translation>Vers~ao</translation>
+    </message>
+    <message>
+        <location filename="plugindialog.cpp" line="14"/>
+        <source>Vendor</source>
+        <translation>Fornecedor</translation>
+    </message>
+</context>
+</TS>
--- trunk/jalapaquery/src/jalapaquery/main.cpp
+++ trunk/jalapaquery/src/jalapaquery/main.cpp
@@ -1,4 +1,7 @@
 #include <QtGui/QApplication>
+#include <QTranslator>
+#include <QLibraryInfo>
+#include <QDebug>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
@@ -12,6 +15,17 @@ int main(int argc, char *argv[])
     QCoreApplication::setApplicationName("jalapaquery");
 
     app.setAttribute(Qt::AA_DontShowIconsInMenus);
+
+    QTranslator qtTranslator;
+    qtTranslator.load("qt_" + QLocale::system().name(),
+                      QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&qtTranslator);
+
+    QTranslator myappTranslator;
+    myappTranslator.load("jalapaquery_" + QLocale::system().name());
+    app.installTranslator(&myappTranslator);
+
+    qDebug() << QLocale::system().name();
 
     MainWindow w;
     w.show();
