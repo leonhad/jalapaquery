c28d385eceb8f733fcce2ed2dcf4898bd073d91d
leonhad@gmail.com <leonhad@gmail.com@b074b155-7ff8-1dec-5e99-c3ab0efeb2e8>
2011-04-19 20:37:57 +0000

    Some text changes

--- jalapaquery/src/jalapaquery/aboutdialog.cpp
+++ jalapaquery/src/jalapaquery/aboutdialog.cpp
@@ -1,11 +1,13 @@
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
+#include "constants.h"
 
 AboutDialog::AboutDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
+    ui->appName->setText(QString(APP_NAME) + " - " + QString(APP_VERSION));
 }
 
 AboutDialog::~AboutDialog()
---
+++ jalapaquery/src/jalapaquery/constants.h
@@ -0,0 +1,7 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+extern const char *APP_VERSION;
+extern const char *APP_NAME;
+
+#endif // CONSTANTS_H
